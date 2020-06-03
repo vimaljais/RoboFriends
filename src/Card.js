f5003fdc14a8a98e166a3a4c9fbef0ce09e74392
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-06-03 21:21:10 +0530

    styled a little

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id}) => {
 	return (
 		<div className='bg-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img src={`https://robohash.org/${id+339}?200x200`} alt='robot' />
+			<img src={`https://robohash.org/${id+99}?200x200`} alt='robot' />
 			<div>
 				<h2> {name} </h2>
 				<p> {email} </p>
---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+		<input
+		 className= 'pa3 ba b--purple bg-lightest-blue' 
+		 type="Search" 
+		 placeholder='Search Robots' />
+		)
+
+}
+
+export default SearchBox;
